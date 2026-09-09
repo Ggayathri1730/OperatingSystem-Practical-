5f7fc92d737eed2bc1c0ca91ad3912a9e3f618e1
Gayathri <gayathriguttikonda2008@gmail.com>
2026-09-09 11:09:23 +0530

    Create execlp.c

---
+++ PRACTIAL CODES/Scheduling algorithms/Processed concept codes/execlp.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    printf("Before exec()\n");
+    execlp("ls", "ls", "-l", NULL);
+    printf("This will not execute.\n");
+    return 0;
+}
